bbbe913a40d0a02739a8eaa3b44b90c0b15944ca
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-02-27 11:42:09 -0800

    Update & modified the web app

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    const zip = req.query.zip;
-    const country = req.query.country;
-    const api = "f72f8608196c78f9b696a0f532e97f24";
-    const url = 'https://pro.openweathermap.org/data/2.5/forecast/climate?zip=${zip},${country}&appid=${api}';
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
     try{
         const result = await axios.get(url);
-        res.render("index.ejs", {
-            zipcode: zip,
-            countrycode: country,
-        });
+        const cocktail = result.data.drinks[0];
+        res.render("index.ejs", {cocktail});
     } catch(error){
-        res.status(500).send(error.message);
+        res.status(500).send("Error fetching cocktail data");
     }
 })
 
